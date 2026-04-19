e47d6892d531706f868620baa64fef71a8bd8dd7
DianaVolf <diana.volf18@gmail.com>
2026-04-19 11:23:04 +0200

    ajout fonction arange, rien testé dans la classe

--- projetDevops/src/main/java/fr/uga/Ndarray.java
+++ projetDevops/src/main/java/fr/uga/Ndarray.java
@@ -1,20 +1,20 @@
 package fr.uga;
 
-public class Ndarray<E>{
+public class Ndarray{
     private int ndim;
     private int[] nshape;
     private int nsize;
-    private E[][] array;
+    private float[][] array;
 
-    private Ndarray(int[] shape, E nombre ){ // pour avoir des fonctions de type zero
+    private Ndarray(int[] shape, float nombre ){ // pour avoir des fonctions de type zero
         if(shape.length>2){
             throw new java.lang.Error("dimension greater than 2");
         }else if (shape.length == 1){
             nsize = shape[0];
-            array = (E[][]) new Object[1][shape[0]];
+            array = new float[1][shape[0]];
         }else{
             nsize = shape[0]*shape[1];
-            array = (E[][]) new Object[shape[0]][shape[1]];
+            array = new float[shape[0]][shape[1]];
         }
         ndim = shape.length;
         nshape = shape;
@@ -31,13 +31,38 @@ public class Ndarray<E>{
         }
     }
 
+    private Ndarray(float[][] a){
+        nsize = a[0].length;
+        nshape = new int[] {nsize};
+        ndim = 1;
+        array = a;
+    }
+
     public int getdim(){return ndim;}
 
     public int[] getnshape(){return nshape;}
 
     public int getsize(){return nsize;}
 
-    public Ndarray<E> zero(int[] shape, E nombre){
-        return new Ndarray<E>(shape, nombre);
+    public Ndarray zero(int[] shape, float nombre){
+        return new Ndarray(shape, nombre);
+    }
+
+    public Ndarray array(float[] tab){
+        float[][]arr = new float[][] {tab};
+        return new Ndarray(arr);
+    }
+
+    public Ndarray arange(float from, float to, float step){
+        float [][] arr = new float[1][(int) ((to - from)/step)];
+        int i = 0;
+        float value = from;
+        while(value<to){
+            arr[0][i]=value;
+            value = value + step;
+            i++;
+        }
+
+        return new Ndarray(arr);
     }
 }
