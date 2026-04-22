d18145cb7406a48e176d3fa7c18b16c780de0be3
Arcefol-alt <Arcefol-alt@users.noreply.github.com>
2026-04-22 12:50:58 +0000

    docs: déploiement automatique du site Maven fd91bd1fbea417dc3a4d91835236be0464561d8b

--- apidocs/type-search-index.js
+++ apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fr.uga","l":"Ndarray"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fr.uga","l":"App"},{"p":"fr.uga","l":"Ndarray"}];updateSearchResults();
